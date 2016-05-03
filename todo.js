d347850578009ee36865fd4127b3395e44384358
jumee <judith.mewes@actano.de>
2016-05-03 16:21:07 +0200

    Add Todo Tree

---
+++ todo.js
@@ -0,0 +1,19 @@
+var Kuchen = {
+    Rezept: 3,
+    Einkaufen: {
+        Supermarkt: {
+            Mehl: 1,
+            Zucker: 1
+        },
+        Bauernhof: {
+            Eier: 1,
+            Milch: {
+                Eimer: 1,
+                Kuh: 1,
+                Melken: 2
+            },
+        },
+    },
+    "Rühren": 2,
+    "Teig kneten": 3
+};
